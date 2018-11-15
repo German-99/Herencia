a1a4b9a3afdf5f3e24dd21a10ee39d880520a0af
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-11-15 08:53:59 -0700

    Agregar archivos de proyecto.

---
+++ Herencia/AnimalTerrestre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class AnimalTerrestre : Animal
+    {
+        public void Caminar()
+        {
+            Console.WriteLine("hola soy un" + Nombre + "y estoy caminando");
+        }
+    }
+}
---
+++ Herencia/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimalAcuatico delfin = new AnimalAcuatico();
+            AnimalTerrestre elefante = new AnimalTerrestre();
+            Kanguro Kanguro = new Kanguro();
+
+            Animal tiburon = new AnimalAcuatico();
+            tiburon.Nombre = "Tiburon";
+            ((AnimalAcuatico)tiburon).Nadar();
+
+            delfin.Nombre = "Delfin";
+            elefante.Nombre = "Elefante";
+
+            delfin.Comer();
+            delfin.Nadar();
+
+            elefante.Caminar();
+
+            Console.ReadLine();
+        }
+    }
+}
